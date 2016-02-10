e9d213c655649e8030f5df4461547dac38a816a5
laripk <larikirby@fastmail.fm>
2016-02-10 23:09:13 +0000

    playing with admin options

--- SpikeDesktopDjango/sddweb/polls/admin.py
+++ SpikeDesktopDjango/sddweb/polls/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Choice, Question
+
+admin.site.disable_action('delete_selected')
+
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
+	#def has_delete_permission(self, request, obj=None): # note the obj=None
+	#	return False
+
+
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None,               {'fields': ['question_text']}),
+		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+	]
+	inlines = [ChoiceInline]
+	list_display = ('question_text', 'pub_date')
+	def has_delete_permission(self, request, obj=None): # note the obj=None
+		return False
+
+admin.site.register(Question, QuestionAdmin)
