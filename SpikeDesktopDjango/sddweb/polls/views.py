481861b1048a205e193eeda3ecb9062407132872
laripk <larikirby@fastmail.fm>
2016-02-11 00:36:44 +0000

    use generic views

--- SpikeDesktopDjango/sddweb/polls/views.py
+++ SpikeDesktopDjango/sddweb/polls/views.py
@@ -1,24 +1,30 @@
-#from django.http import HttpResponse #only needed for stub views (eg, return almost plain string)
-from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
 
-def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	context = {'latest_question_list': latest_question_list}
-	return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html' # instead of "polls/question_list.html"
+	context_object_name = 'latest_question_list' # instead of "question_list"
 
-def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/detail.html', {'question': question})
+	def get_queryset(self):
+		"""Return the last five published questions."""
+		return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/results.html', {'question': question})
-	
+
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html' # instead of "polls/question_detail.html"
+
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html' # instead of "polls/question_detail.html"
+
+
 def vote(request, question_id):
 	p = get_object_or_404(Question, pk=question_id)
 	try:
