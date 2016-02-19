b98669479099e69a20882c91de211d543f3b86f0
laripk <larikirby@fastmail.fm>
2016-02-19 04:32:25 +0000

    fix comtypes

---
+++ tests/keywordtest.py
@@ -0,0 +1,8 @@
+import keyword
+
+print("True is a keyword is", keyword.iskeyword("True"))
+print("False is a keyword is", keyword.iskeyword("False"))
+print("true is a keyword is", keyword.iskeyword("true"))
+print("false is a keyword is", keyword.iskeyword("false"))
+print("True_ is a keyword is", keyword.iskeyword("True_"))
+print("False_ is a keyword is", keyword.iskeyword("False_"))
